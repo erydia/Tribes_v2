4a1c13ce736feafcd9d8ac002341c1f1a4de7b00
Dominika Smaza <dominikasmaza47@gmail.com>
2018-10-29 18:55:43 +0100

    refactor javascript to typescript

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // Zmienne które zajmują się przechowywaniem wartości czasu.
-    let time:number = 0;
-    let seconds:number | string = 0;
-    let minutes:number | string = 0;
-    let hours:number | string = 0;
+    let time: number = 0;
+    let seconds: number = 0;
+    let minutes: number = 0;
+    let hours: number = 0;
 
     // Pobieranie elementów reprezentujących wyniki z drzewa DOM. 
-    const secondEl = document.querySelector('.second');                                 // <----   :Element ?
-    const minuteEl = document.querySelector('.minute');
-    const hourEl = document.querySelector('.hour');
+    const secondEl: HTMLElement = document.querySelector('.second');
+    const minuteEl: HTMLElement = document.querySelector('.minute');
+    const hourEl: HTMLElement = document.querySelector('.hour');
 
     // Zegar który tyka co 1000ms. 
-    const clock = setInterval(() => { 
+    const clock: NodeJS.Timeout = setInterval(() => { 
         // Zwiększanie się ilości czasu.
         time = time + 1;
         // Wyliczanie sekund poprzez dzielenie modulo.
@@ -24,68 +24,68 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // If - wykona się wtedy gdy ilość sekund będzie mniejsza niż 10.
         if (seconds < 10) {
-            seconds = `0${seconds}`;
+            secondEl.innerText = String(`0${seconds}`)
         };
         // If - wykona się wtedy gdy ilość minut będzie mniejsza niż 10.
         if (minutes < 10) {
-            minutes = `0${minutes}`;
+            minuteEl.innerText = String(`0${minutes}`);
         }; 
         // If - wykona się wtedy gdy ilość godzin będzie mniejsza niż 10.
         if (hours < 10) {
-            hours = `0${hours}`;
+            hourEl.innerText = String(`0${hours}`);
         }; 
         // Aktualizacja czasu w htmlu.
-        secondEl.innerText = seconds;                       // <---- Jak to obejść?
-        minuteEl.innerText = minutes;
-        hourEl.innerText = hours;
+        secondEl.innerText = String(seconds); 
+        minuteEl.innerText = String(minutes);
+        hourEl.innerText = String(hours);
     },1000);
 
-    interface materialsObject {
+    interface IMaterialsObject {
         wood: number;
         stone: number;
         gold: number;
-    }
+    };
 
     //Ilość surowców.
-    const materials: materialsObject= {
+    const materials: IMaterialsObject= {
         wood: 0,
         stone: 0,
         gold: 0,
     };
 
     //Przychód poszczególnych surowców.
-    const incomeMaterials:materialsObject  = {
+    const incomeMaterials: IMaterialsObject  = {
         wood: 1,
         stone: 1,
         gold: 1,
     };
 
-    interface storageCapacity {
+    interface IStorageCapacity {
         wood: number;
         stone: number;
-    }
+    };
 
     // Pojemnośc magazynów.
-    const storageCapacity:storageCapacity  = {
+    const storageCapacity: IStorageCapacity  = {
         wood: 20,
         stone: 20,
     };
 
     // Zmienne, które przechowuje ilość robotników.
-    let worker:number = 5;
+    let worker: number = 5;
     // Zmienne, które przechowują maksymalną ilość robotników.
-    const maxWorker:number = 10;
+    const maxWorker: number = 10;
 
     // Pobieranie elementów reprezentujących wyniki z drzewa DOM.
-    const woodEl = document.querySelector('.wood');
-    const stoneEl = document.querySelector('.stone');
-    const goldEl = document.querySelector('.gold');
-    const workerEl = document.querySelector('.worker');
-    const maxWoodEl = document.querySelector('.max-wood');
-    const maxStoneEl = document.querySelector('.max-stone');
+    const woodEl: HTMLElement = document.querySelector('.wood');
+    const stoneEl: HTMLElement = document.querySelector('.stone');
+    const goldEl: HTMLElement = document.querySelector('.gold');
+    const workerEl: HTMLElement = document.querySelector('.worker');
+    const maxWoodEl: HTMLElement = document.querySelector('.max-wood');
+    const maxStoneEl: HTMLElement = document.querySelector('.max-stone');
 
     // Zwiększanie się ilości drewna i kamienia co 1000ms.
-    const intervalWoodStoneIncome = setInterval(() => {
+    const intervalWoodStoneIncome: NodeJS.Timeout = setInterval(() => {
         // Zatrzymanie przychodu drewna i kamienia na koniec gry.
         if (endOfTheGame) {
             clearInterval(intervalWoodStoneIncome);
@@ -97,7 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
             if (materials.wood >= storageCapacity.wood) { 
                 materials.wood = storageCapacity.wood;
             };
-            woodEl.innerText = materials.wood; 
+            woodEl.innerText = String(materials.wood); 
         }    
         // Zwiększenie ilości kamienia gdy jest możliwe jego wyprodukowanie.
         if (materials.stone < storageCapacity.stone) {
@@ -106,23 +106,23 @@ document.addEventListener('DOMContentLoaded', () => {
             if (materials.stone >= storageCapacity.stone) { 
                 materials.stone = storageCapacity.stone;
             }; 
-            stoneEl.innerText  = materials.stone;
+            stoneEl.innerText  = String(materials.stone);
         };
     },1000);
 
     // Zwiększanie ilość złota co 3000ms.
-    const intervalGoldIncome = setInterval(() => { 
+    const intervalGoldIncome: NodeJS.Timeout = setInterval(() => { 
         // Zatrzymanie przychodu złota.
         if (endOfTheGame) {            
             clearInterval(intervalGoldIncome);
         };
         // Zwiększenie się ilości złota.
         materials.gold = materials.gold + incomeMaterials.gold;
-        goldEl.innerText = materials.gold; 
+        goldEl.innerText = String(materials.gold); 
     },3000);
 
     // Dodawanie określonych bonusów po wybudowaniu budynków.
-    const addBonus:Function = (value:string) => {
+    const addBonus: Function = (value: string) => {
         // Zwiększenie przychodu drewna.
         if (value === 'woodIncome') {   
             incomeMaterials.wood += 2;
@@ -136,16 +136,16 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (value === 'newWorker') {
             if (worker < maxWorker) {
                 worker += 1;
-                workerEl.innerText = worker;
+                workerEl.innerText = String(worker);
             };
         // Powiększenie magazynu drewna.    
         } else if (value === 'enlargingTheWarehouseWood') {
             storageCapacity.wood += 10;
-            maxWoodEl.innerText = storageCapacity.wood;
+            maxWoodEl.innerText = String(storageCapacity.wood);
         // Powiększenie magazynu kamienia.    
         } else if (value === 'enlargingTheWarehouseStone') {
             storageCapacity.stone += 10;
-            maxStoneEl.innerText = storageCapacity.stone;
+            maxStoneEl.innerText = String(storageCapacity.stone);
         // Zwycięstwo.    
         } else if (value === 'victory') {
             victory();
@@ -158,12 +158,12 @@ document.addEventListener('DOMContentLoaded', () => {
     let endOfTheGame:boolean = false;
 
     // Pobieranie elementu reprezentującego wynik z drzewa DOM. 
-    const boardEl= document.querySelector('.board');
+    const boardEl: HTMLElement = document.querySelector('.board');
 
     // Fukcja, która wyświetla nowy budynek w htmlu.
-    const construction = (type) => {
+    const construction = (type: string) => {
         // Tworzenie nowego diva, który stanie się budynkiem.
-        const newBuilding:HTMLDivElement = document.createElement('div');
+        const newBuilding: HTMLElement = document.createElement('div');
         // Przypisanie mu odpowiednich klas.
         newBuilding.classList.add(type);
         newBuilding.classList.add('new-building');
@@ -172,11 +172,11 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // Pobieranie elementów reprezentujących wyniki z drzewa DOM. 
-    const timerEl = document.querySelector('.timer');
-    const buildingBoxEl = document.querySelector('.building-box');
+    const timerEl: HTMLElement = document.querySelector('.timer');
+    const buildingBoxEl: HTMLElement  = document.querySelector('.building-box');
 
     // Wybuduj budynek.
-    const constructionBuilding:Function = (object) => {
+    const constructionBuilding: Function = (object) => {
         // Nastąpi rozpoczęcie budowy.
         if ( materials.wood >= object.wood && materials.stone >= object.stone && materials.gold >= object.gold && setConstructionTimer === false && endOfTheGame === false) {
             // Włączenie licznika budowy.
@@ -193,9 +193,9 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // Funkcja, która zwraca czas budowy poszczególnych budynków.
-    const timerInProgress = (time) => {
+    const timerInProgress: Function = (time: number) => {
         // Zmienna która przechowuje czas budowy.
-        const constructionTime = Math.floor(time / worker);
+        const constructionTime: number = Math.floor(time / worker);
         // Pojawienie się komunikatu o czasie budowy w htmlu.
         timerEl.innerText = `Czas budowy: ${constructionTime}`;  
         buildingBoxEl.classList.add('opacity-05');
@@ -204,8 +204,8 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // Funkcja, która wyświetla czas budowy budynku w htmlu.
-    const constructionOfTheBuilding:Function = (constructionTime:number,  value:string) => {
-        const contructionInProgress = setInterval(() => {
+    const constructionOfTheBuilding: Function = (constructionTime: number,  value: string) => {
+        const contructionInProgress: NodeJS.Timeout = setInterval(() => {
             // Zmniejszanie się czasu budowy co 1000ms.
             constructionTime--;
             // Aktualizacja czasu budowy w htmlu.
@@ -228,11 +228,11 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // Pobieranie elementów reprezentujących wyniki z drzewa DOM. 
-    const winBoxEl = document.querySelector('.win-box');
-    const infoBoxEl  = document.querySelector('.info-box');
+    const winBoxEl: HTMLElement = document.querySelector('.win-box');
+    const infoBoxEl: HTMLElement  = document.querySelector('.info-box');
 
     // Funkcja, która informuje gracza o wygraniu gry.
-    const victory:Function = () => {
+    const victory: Function = () => {
         // Zmiana wartości zmiennej, która przechowuje informacje o tym czy gra została wygrana.
         endOfTheGame = true;
         // Zatrzymanie czasu.
@@ -243,27 +243,27 @@ document.addEventListener('DOMContentLoaded', () => {
         infoBoxEl.classList.add('opacity-05');
   
         // Pobieranie elementu reprezentującego wynik z drzewa DOM. 
-        const newBuildingEls = document.querySelectorAll('.new-building');     /// <---- co tutaj? Array nie pasuje
+        const newBuildingEls: NodeListOf<Element> = document.querySelectorAll('.new-building');
         // Dodanie do każdego elementu stylu poprzez pętle. 
         newBuildingEls.forEach((el) => {
             el.classList.add('opacity-05');
         });
-    }
+    };
  
-
-    interface building {                               
+    interface IBuilding {                               
         name: string;
         wood: number;
         stone: number;
         gold: number;
         time: number;
         bonus: string;
-    }
+    };
 
     // Zbiór informacji na temat budynków.                    
-    const buildings = [{                                 // Czy tutaj powinnam przerobić strukture by podpiąć pod każdy budynek jeden interface? 
-        name: 'sawmill',                                 // Czy jest jakieś inne rozwiązanie?  
-        wood: 20,                                        // Czy pisanie js'a w ts'cie różni się jakoś? Prócz nadawania typów itp. Zastanawiam się czy inaczej powinnam podchodzić do kodu
+    // const buildings: Buildings  = [{                
+    const buildings: IBuilding[]  = [{                    
+        name: 'sawmill',                                 
+        wood: 20,                                        
         stone: 0,                                           
         gold: 3,
         time: 120,
@@ -313,13 +313,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }];
 
     // Pobieranie elementów reprezentujących wyniki z drzewa DOM.
-    const buttonSawmillEl = document.querySelector('.button-sawmill');
-    const buttonQuarryEl = document.querySelector('.button-quarry');
-    const buttonGoldMineEl = document.querySelector('.button-gold-mine');
-    const buttonFarmEl = document.querySelector('.button-farm');
-    const buttonWoodWarehouseEl = document.querySelector('.button-wood-warehouse');
-    const buttonStoneWarehouseEl = document.querySelector('.button-stone-warehouse');
-    const buttonCastleEl = document.querySelector('.button-castle');
+    const buttonSawmillEl: HTMLElement = document.querySelector('.button-sawmill');
+    const buttonQuarryEl: HTMLElement = document.querySelector('.button-quarry');
+    const buttonGoldMineEl: HTMLElement = document.querySelector('.button-gold-mine');
+    const buttonFarmEl: HTMLElement = document.querySelector('.button-farm');
+    const buttonWoodWarehouseEl: HTMLElement = document.querySelector('.button-wood-warehouse');
+    const buttonStoneWarehouseEl: HTMLElement = document.querySelector('.button-stone-warehouse');
+    const buttonCastleEl: HTMLElement = document.querySelector('.button-castle');
 
     // Wybuduj TARTAK.  
     buttonSawmillEl.addEventListener('click', () => {
@@ -357,7 +357,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
-
-
-
-
